a8bd6b4d77d6e6a1f7487c70cadb917c4b3d5f37
Collin Dahlback <cdahlback208@icloud.com>
2023-02-05 20:10:38 -0600

    refactored code to fit previous commit
    - changed url validation method to prev team
    - refactored code around that change

--- data_extraction.py
+++ data_extraction.py
@@ -9,22 +9,16 @@ from itertools import repeat
 
 def extract_phone_data(url):
     t0 = time()
-    # Either gets the response from the url, or prints the error sent
-    try:
-        response = requests.get(url)
-        # use the response to create a soup (full html of the url)
-        soup = bs4.BeautifulSoup(response.content, "html.parser")
-    except Exception as e:
-        print(str(e))
-        print("URL {0} not found".format(url))
-        # this is only so we don't finish the method call
-        return -1
+    # get the html
+    response = requests.get(url)
+    soup = bs4.BeautifulSoup(response.content, "html.parser")
 
     # Extract phone numbers from soup using regex for phone numbers (need to modify re so it catches 5074401234)
     phone_numbers = []
     for tag in soup.find_all(text=re.compile(r'\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}')):
         phone_numbers.append(tag.string)
 
+    # print phone numbers from url
     print("URL: {0} data \n".format(url))
     print("PHONE NUMBERS:")
     for number in phone_numbers:
@@ -33,6 +27,7 @@ def extract_phone_data(url):
         if len(number) < 15:
             print(number)
 
+    # print time taken for specific url
     t1 = time() - t0
     print("TIME ELAPSED: {:0.4f} sec".format(t1))
     print("------------------------")
@@ -40,14 +35,9 @@ def extract_phone_data(url):
 
 def extract_email_data(url):
     t0 = time()
-    # Either gets the response from the url, or prints the error sent
-    try:
-        response = requests.get(url)
-        soup = bs4.BeautifulSoup(response.content, "html.parser")
-    except Exception as e:
-        print(str(e))
-        print("URL {0} not found".format(url))
-        return -1
+    # get the html
+    response = requests.get(url)
+    soup = bs4.BeautifulSoup(response.content, "html.parser")
 
     # Extract email addresses
     email_addresses = []
@@ -57,11 +47,13 @@ def extract_email_data(url):
             if 'mailto:' in email:
                 email_addresses.append(email)
 
+    # print emails for each url
     print("URL: {0} data \n".format(url))
     print("EMAILS:")
     for email in email_addresses:
         print(email)
 
+    # display time took for specific url to extract data
     t1 = time() - t0
     print("TIME ELAPSED: {:0.4f} sec".format(t1))
     print("------------------------")
@@ -106,27 +98,35 @@ def status_code(url, headers, timeout):
         return -1
 
 
+# open input file
 inputFile = open("txt_files/urls_with_no_email.txt", "r")
 inputReader = csv.reader(inputFile)
 
+# extract all urls from file, place them in list for our url validator
 url_list = []
 for row in inputReader:
     if row[0] == "1000012166":
         break
     url_list.append(row[1])
 
+# get status code for each url
 status_codes = get_statuscode(url_list)
 for code in status_codes:
     print(code)
 
 t0 = time()
 times = []
-for url in url_list:
-    t = extract_email_data(url)
+for i in range(len(url_list) - 1):
+    # if this url returned a -1, 400 we don't need to check it
+    if status_codes[i] != 200:
+        continue
+    t = extract_email_data(url_list[i])
     # makes sure the time is positive ( since we return -1 if the url doesn't open )
     if t > -1:
         times.append(t)
+
 t1 = time() - t0
 print("TOTAL TIME: {0} seconds for (30) urls ran".format(t1))
 print(times)
+
 inputFile.close()
