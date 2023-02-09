879f5048e3b929c92f7c4863914de9721a738174
Collin Dahlback <cdahlback208@icloud.com>
2023-02-09 15:54:21 -0600

    Code Clean up
    -Removed code aside from functions
    -Reason? Moving to main function
    -Also cleaned up all files

--- create_urls_from_emails.py
+++ create_urls_from_emails.py
@@ -1,11 +1,18 @@
 import csv
 import re
-import pandas as pd
-import validators       # Package that can validate URLs and emails with call to validate.url() or .email()
-
-import ThreadPoolExecutorPlus
-from itertools import repeat
 import requests
+import ThreadPoolExecutorPlus
+import pandas as pd
+
+from elis_functions import cleanEmail
+from itertools import repeat
+
+# list of domain names we don't want
+bad_domain_names = ['yahoo.com', 'gmail.com', "hotmail.com", "icloud.com", "comcast.net", "GMAIL.COM",
+                    "outlook.com", "msn.com", "arvig.net", "charter.net", "winona.edu", "aol.com"]
+
+# regex to detect valid email (works great so far, may need building upon
+email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
 
 
 def get_statuscode(df):
@@ -46,99 +53,19 @@ def status_code(url, id, headers, timeout):
     except:
         return id, -1
 
-# I want to take "emails_no_url.txt" as an input
-# Iterate through all the emails
-#   extract the domain name
-#   build url around domain name
-#   see what request our url gives
-# add good urls to "successful_urls.txt"
-
-# list of domain names we don't want
-from elis_functions import cleanEmail
-
-bad_domain_names = ['yahoo.com', 'gmail.com', "hotmail.com", "icloud.com", "comcast.net", "GMAIL.COM",
-                    "outlook.com", "msn.com", "arvig.net", "charter.net", "winona.edu", "aol.com"]
-
-# regex to detect valid email (works great so far, may need building upon
-regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
-
 
 def build_url(email):
     """
     :param email: email to build url from
-    :return:
-    If our email matches the regex and is not in our list of invalid domain names
-        - Return complete URL ready to test
-    If our email matches the regex, but is in our list of invalid domain names
-        - Return empty string, to let the test_url function know to disregard it
-    Else in case of not matching regex,
-        - we return the full text of the email to investigate why we got a bad email from our extract_email function
+    :return: Formatted url extracted from email
     """
-    if re.fullmatch(regex, cleanEmail(email)):
+    if re.fullmatch(email_regex, cleanEmail(email)):
         domain_name = email.split("@")[-1]
         if domain_name not in bad_domain_names:
             return "https://www.{0}/".format(domain_name)
 
-# create dataframe
-data = pd.read_csv("data/mn_bbb_businesses.csv", low_memory=False)
 
-emailsNoURL = data.loc[(data['Email'].notna()) & (data['Website'].isna()) & (data['BBBID'] != 704)][['BusinessID', 'Email']]
-# print(emailsNoURL)
+# emailsNoURL = data.loc[(data['Email'].notna()) & (data['Website'].isna())][['BusinessID', 'Email']]
 # URLsNoEmail = data.loc[(data['Website'].notna()) & (data['Email'].isna()) & (data['BBBID'] == 704)][['BusinessID', 'Website']]
-# print(URLsNoEmail)
 # URLsNoPhone = data.loc[(data['Website'].notna()) & (data['Phone'].isna()) & (data['BBBID'] == 704)][['BusinessID', 'Website']]
-# print(URLsNoPhone)
-
-
-# extract URLs for all emails
-extractedURLs = emailsNoURL
-extractedURLs["Website"] = emailsNoURL['Email'].apply(lambda email: build_url(email))
-successfulURLs = extractedURLs.loc[extractedURLs['Website'].notna()]
-# print(successfulURLs)
-unsuccessfulURLs = extractedURLs.loc[extractedURLs['Website'].isna()]
-# print(unsuccessfulURLs)
-from time import time
-
-t0 = time()
-statusCodeDF = get_statuscode(successfulURLs)
-t1 = time() - t0
-print(t1)
-
-new_df = pd.merge(successfulURLs, statusCodeDF, how='inner')
-
-new_df = new_df.loc[(new_df['StatusCode'] != 404) & (new_df['StatusCode'] != -1) & (new_df['StatusCode'] != 403)]
-
-new_df.to_csv('good_emails.csv')
-# input file from where we get our emails
-# ipt_file = open("txt_files/emails_with_no_url.txt", "r")
-# ipt_reader = csv.reader(ipt_file)
-#
-# # Writer to the "successful_extracted_urls.txt"
-# opt_file = open("txt_files/successful_extracted_urls.txt", "w")
-# out_writer = csv.writer(opt_file)
-#
-# # Writer to the "unsuccessful_emails_to_extract_urls.txt"
-# opt_file1 = open("txt_files/unsuccessful_emails_to_extract_urls.txt", "w")
-# out_writer1 = csv.writer(opt_file1)
-
-# iterate over ipt file
-# for line in ipt_reader:
-#     # place the business id you want to stop at (this will change once threading is introduced)
-#     if line[0] == "1000003569":
-#         break
-#     # save email and business id
-#     email = line[1]
-#     business_id = line[0]
-#     # get either an empty string (didn't pass build_url) or a successfully built url
-#     url = build_url(email)
-#     # make sure the url is not empty before writing it to the output file
-#     if validators.url(url):
-#         out_writer.writerow([business_id, url])
-#     else:
-#         out_writer1.writerow([business_id, url])
-
-# close files
-# ipt_file.close()
-# opt_file.close()
-# opt_file1.close()
 
--- data_extraction.py
+++ data_extraction.py
@@ -8,40 +8,51 @@ from itertools import repeat
 import pandas as pd
 from get_status_codes import get_statuscode
 
-def extract_phone_data(url):
-    t0 = time()
-    # get the html
-    response = requests.get(url)
-    soup = bs4.BeautifulSoup(response.content, "html.parser")
+
+def extract_phone_data(id, url):
+    """
+    Function to find phone numbers
+    :param id: id associated with extracted phone #s
+    :param url: url associated with extracted phone #s
+    :return: dictionary of all phone #s associated with a business
+    """
+    try:
+        response = requests.get(url)
+        soup = bs4.BeautifulSoup(response.content, "html.parser")
+    except Exception as e:
+        print(str(e))
+        print("bad url")
+        return None
 
     # Extract phone numbers from soup using regex for phone numbers (need to modify re so it catches 5074401234)
-    phone_numbers = []
+    phone_numbers = {'BusinessID': id}
+    counter = 0
     for tag in soup.find_all(text=re.compile(r'\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}')):
-        phone_numbers.append(tag.string)
+        counter += 1
+        phone_numbers["Phone#{0}:".format(counter)] = tag.string
 
-    # print phone numbers from url
-    print("URL: {0} data \n".format(url))
-    print("PHONE NUMBERS:")
-    for number in phone_numbers:
-        # this if statements should not be needed
-        # I added it since the regex sometimes detects the full text around the phone number, not the number itself.
-        if len(number) < 15:
-            print(number)
-
-    # print time taken for specific url
-    t1 = time() - t0
-    print("TIME ELAPSED: {:0.4f} sec".format(t1))
-    print("------------------------")
+    if len(phone_numbers) >= 1:
+        return phone_numbers
 
 
 def extract_email_data(id, url):
+    """
+    Function to find emails
+    :param id: id associated with extracted emails
+    :param url: url associated with extracted emails
+    :return: dictionary of all emails associated with a business
+    """
     t0 = time()
     # get the html
     # print(id)
     # print(url)
-
-    response = requests.get(url)
-    soup = bs4.BeautifulSoup(response.content, "html.parser")
+    try:
+        response = requests.get(url, timeout=5)
+        soup = bs4.BeautifulSoup(response.content, "html.parser")
+    except Excpetion as e:
+        print(str(e))
+        print("bad url")
+        return None
 
     # Extract email addresses
     email_addresses = {'BusinessID': id}
@@ -50,62 +61,11 @@ def extract_email_data(id, url):
         email = tag.get('href')
         if email:
             if 'mailto:' in email:
+                if email in email_addresses:
+                    continue
                 email_number += 1
                 email_addresses['Email' + str(email_number)] = email
     if len(email_addresses) > 1:
         return email_addresses
 
-    # # print emails for each url
-    # print("URL: {0} data \n".format(url))
-    # print("EMAILS:")
-    # for email in email_addresses:
-    #     print(email)
-    #
-    # # display time took for specific url to extract data
-    # t1 = time() - t0
-    # print("TIME ELAPSED: {:0.4f} sec".format(t1))
-    # print("------------------------")
-    # return round(t1, 5)
 
-
-# open input file
-# inputFile = open("txt_files/urls_with_no_email.txt", "r")
-# inputReader = csv.reader(inputFile)
-
-# pandas
-data = pd.read_csv("good_emails.csv", low_memory=False)
-# print(extract_email_data(data))
-result = [extract_email_data(id, url) for id, url in zip(data['BusinessID'], data['Website']) if extract_email_data(id, url) != None]
-print(result)
-# URLsNoEmail = data.loc[(data['Website'].notna()) & (data['Email'].isna()) & (data['BBBID'] == 704)][['BusinessID', 'Website']]
-
-# urlList = URLsNoEmail['Website'].values[:100]
-
-# extract all urls from file, place them in list for our url validator
-# url_list = []
-# for row in inputReader:
-#     if row[0] == "1000012166":
-#         break
-#     url_list.append(row[1])
-#
-# # get status code for each url
-# status_codes = get_statuscode(url_list)
-# for code in status_codes:
-#     print(code)
-#
-# t0 = time()
-# times = []
-# for i in range(len(url_list) - 1):
-#     # if this url returned a -1, 400 we don't need to check it
-#     if status_codes[i] != 200:
-#         continue
-#     t = extract_email_data(url_list[i])
-#     # makes sure the time is positive ( since we return -1 if the url doesn't open )
-#     if t > -1:
-#         times.append(t)
-#
-# t1 = time() - t0
-# print("TOTAL TIME: {0} seconds for (30) urls ran".format(t1))
-# print(times)
-
-# inputFile.close()
