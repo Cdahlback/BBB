40bd9ef955cdda7e0b729980c3e9254da65c0bd5
bam-maker <123272575+bam-maker@users.noreply.github.com>
2023-03-20 20:55:45 -0700

    update

--- url_only.py
+++ url_only.py
@@ -24,24 +24,55 @@
 # complete = pd.DataFrame(dict_for_pandas)
 
 # complete.to_csv('data/biruk_results.csv')
-import csv
 
-# specify the input and output file names
-input_file = "yagooglesearch.py.log"
-output_file = "data/biruk_results.csv"
+# import csv
 
-# open the input and output files
-with open(input_file, "r") as in_file, open(output_file, "w", newline="") as out_file:
-    # create a CSV writer for the output file
-    csv_writer = csv.writer(out_file)
+# # specify the input and output file names
+# input_file = "yagooglesearch.py.log"
+# output_file = "data/biruk_results.csv"
 
-    # loop over each line in the input file
-    for line in in_file:
-        # check if the line contains "Found unique URL"
-        if "Found unique URL" in line:
-            # extract the URL from the line
-            url = line[line.index("http"):].strip()
+# # open the input and output files
+# with open(input_file, "r") as in_file, open(output_file, "w", newline="") as out_file:
+#     # create a CSV writer for the output file
+#     csv_writer = csv.writer(out_file)
 
-            csv_writer.writerow([url])
+#     # loop over each line in the input file
+#     for line in in_file:
+#         # check if the line contains "Found unique URL"
+#         if "Found unique URL" in line:
+#             # extract the URL from the line
+#             url = line[line.index("http"):].strip()
+
+#             csv_writer.writerow([url])
+
+# import csv
+
+# with open('data/biruk.csv', mode='r') as input_file, open('output.csv', mode='w', newline='') as output_file:
+#     reader = csv.DictReader(input_file)
+#     writer = csv.DictWriter(output_file, fieldnames=['BusinessID', 'BusinessName'])
+#     writer.writeheader()
+
+#     for row in reader:
+#         business_id = row['BusinessID']
+#         business_name = row['BusinessName']
+#         writer.writerow({'BusinessID': business_id, 'BusinessName': business_name})
+
+import pandas as pd
+
+# Load the first CSV file
+df1 = pd.read_csv('data/biruk_results.csv')
+
+# Load the second CSV file
+df2 = pd.read_csv('output.csv')
+
+# Merge the two dataframes based on a common column
+merged_df = pd.merge(df1, df2, on='BusinessID,BusinessName,URL')
+
+# Save the merged dataframe to a new CSV file
+merged_df.to_csv('merged_file.csv', index=False)
+
+
+
+
 
 
