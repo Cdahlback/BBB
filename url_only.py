6881e31ad992eb829e9339a949c8c5d3d9ab6178
bam-maker <123272575+bam-maker@users.noreply.github.com>
2023-03-20 19:45:35 -0700

    update

--- url_only.py
+++ url_only.py
@@ -1,26 +1,40 @@
+# import re
+# import pandas as pd
+
+# true_list = []
+# business_IDs = []
+# websites = []
+
+# terminal_txt = open('yagooglesearch.py.log', 'r')
+# for line in terminal_txt:
+#     if re.match(r'^\d{8}.*', line):
+#         true_list.append(line)
+
+# for entry in true_list:
+#     delete = re.sub(r'\n', '', entry)
+#     split = re.split(r' ', delete)
+#     if len(split) == 1:
+#         business_IDs.append(split[0])
+#         websites.append('')
+#     else:
+#         business_IDs.append(split[0])
+#         websites.append(split[1])
+
+# dict_for_pandas = {"BusinessID": business_IDs, "Website": websites}
+# complete = pd.DataFrame(dict_for_pandas)
+
+# complete.to_csv('data/biruk_results.csv')
+import csv
 import re
-import pandas as pd
 
-true_list = []
-business_IDs = []
-websites = []
+# Open log file and create output CSV file
+with open('yagooglesearch.py.log', 'r') as log_file, open('data/biruk_results.csv', 'w', newline='') as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerow(['URL'])  # Write header row to CSV file
 
-terminal_txt = open('yagooglesearch.py.log', 'r')
-for line in terminal_txt:
-    if re.match(r'^\d{8}.*', line):
-        true_list.append(line)
-
-for entry in true_list:
-    delete = re.sub(r'\n', '', entry)
-    split = re.split(r' ', delete)
-    if len(split) == 1:
-        business_IDs.append(split[0])
-        websites.append('')
-    else:
-        business_IDs.append(split[0])
-        websites.append(split[1])
-
-dict_for_pandas = {"BusinessID": business_IDs, "Website": websites}
-complete = pd.DataFrame(dict_for_pandas)
-
-complete.to_csv('data/biruk_results.csv')
+    # Iterate through each row in log file
+    for row in log_file:
+        # Use regular expression to find URLs in row
+        urls = re.findall(r'(https?://\S+)', row)
+        for url in urls:
+            writer.writerow([url])  # Write URL to CSV file
