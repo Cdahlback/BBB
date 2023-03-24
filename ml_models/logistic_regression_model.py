c8afc850899d1bccac8f49c87aab995ce5aed176
Dylan Moore <100745383+dmoore3554@users.noreply.github.com>
2023-03-23 23:56:24 -0500

    Updated model

--- ml_models/logistic_regression_model.py
+++ ml_models/logistic_regression_model.py
@@ -4,25 +4,31 @@ from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 
 # Read dataset
-data = pd.read_csv("FILE HERE")
+data = pd.read_csv("C:/Users/nicky/Documents/GitHub/BBB/data/filled_ind_var.csv")
 
 model_data = data[:1600]
 
 holdout_data = data[1600:]
 
-# Show correlation between variables
-print(model_data.corr())
-
-# Create Independent and Dependent variable sets
-Y = model_data["manually_checked"].values
-
 variables = ["contains_contacts_page",
              "contains_business_name",
-             "contains_business_name_in_copyright",
              "contains_social_media_links",
-             "contains_reviews_page"]
+             "url_contains_email",
+             "BBBRatingScore",
+             "IsBBBAccredited"]
 
-X = model_data[:, vars].values
+# Show correlation between variables
+corr_data = data.loc[:, variables + ["manually_checked"]]
+
+pd.set_option('display.max_columns', None)
+print(corr_data.corr()[-1:])
+# Create Independent and Dependent variable sets
+Y = model_data["manually_checked"].values
+print(Y)
+
+
+X = model_data.loc[:, variables].values
+print(X)
 
 # Split data into testing and training sets
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=1)
@@ -34,8 +40,7 @@ website_relation_model.fit(x_train, y_train)
 # Checking the predictive power of the model using the testing sets
 y_prediction = website_relation_model.predict(x_test)
 
-print()
-print("Coefficients: \n", website_relation_model.coef_)
+print("\nCoefficients: \n", website_relation_model.coef_)
 
 print("Root mean squared error: %.2f" % mean_squared_error(y_test, y_prediction, squared=False))
 
