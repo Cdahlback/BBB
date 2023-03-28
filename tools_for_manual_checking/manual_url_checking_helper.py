e2c49506106e97469c550dc6b8ba2f651993d44c
Dylan Moore <100745383+dmoore3554@users.noreply.github.com>
2023-03-27 19:43:36 -0500

    Update manual_url_checking_helper.py

--- tools_for_manual_checking/manual_url_checking_helper.py
+++ tools_for_manual_checking/manual_url_checking_helper.py
@@ -1,32 +1,36 @@
 from selenium import webdriver
 import pandas as pd
-import random
-import numpy as np
 
-"""
-MUST CLEAN ALL URLS PRIOR TO RUNNING THIS SCRIPT
-Used to help assist our team in automating the manual checking process
-Uses selenium to open webpages
-"""
+"""MUST CLEAN ALL URLS PRIOR TO RUNNING THIS SCRIPT"""
 
-df = pd.read_csv('data/mn_bbb_businesses.csv', low_memory=False)
+df = pd.read_csv('../data/data_to_be_checked.csv', low_memory=False)
 counter = 0
 browser = webdriver.Chrome()
 
-s = set()
+
 
 while counter < 400:
-    row_index = random.randint(1, len(df) - 1)
-    row = df.loc[row_index]
+    row = df.loc[counter]
     business_name = row["BusinessName"]
+    address = row['StreetAddress']
+    city = row['City']
+    TOB = row['TOBDescription']
     url = row["Website"]
-    if url in s or url is np.nan:
-        continue
-    else:
-        counter += 1
-        s.add(url)
-        browser.get(url)
-        associated = input("Enter 1 if associated with {0}, 0 if not".format(business_name))
-        df.loc[row_index, "manually_checked"] = associated
+    browser.get(url)
+    associated = input("Enter 1 if associated 0 if not.\n"
+                       "Business name: {0}\n"
+                       "Address: {1}, {2}\n"
+                       "Type of Business: {3}\n".format(business_name, address, city, TOB))
+    df.loc[counter, "manually_checked"] = associated
+    df.to_csv('data_to_be_checked.csv')
+    counter += 1
 
 
+# Psudo
+# add a new column to the datafram (checked_manually), set all to nan
+# Randomly pick a row from the dataframe
+# check if we have got this row before, continue if so
+# Extract the name and website for that row
+# Open the url with selenium
+# wait for a user input (0 or 1)
+# update the column checked_manually with the result (0 or 1)
