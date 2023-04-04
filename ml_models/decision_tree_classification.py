58fde9b36fcdaa2d249b535bc5c0ae2882b0cd3d
Collin Dahlback <cdahlback208@icloud.com>
2023-04-04 08:54:04 -0500

    Ran some stats on decisionTreeClassification
    got random permutation of variables and accuracy for each of those permutations.

--- ml_models/decision_tree_classification.py
+++ ml_models/decision_tree_classification.py
@@ -1,8 +1,11 @@
+import random
+
 import pandas as pd
 from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from ml_models.Vizualization.ClassificationVisualization import *
 
 # Load the iris dataset
 
@@ -23,22 +26,44 @@ variables = [
     "IsBBBAccredited"
 ]
 
-# Create features and output
-X = data[variables].values
-y = data['manually_checked'].values
 
-# Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+def test_ipts_decision_tree(data, variables, r_s, ml_stats_df):
 
-# Create a decision tree classifier
-clf = DecisionTreeClassifier()
+    # Create features and output
+    X = data[variables].values
+    y = data['manually_checked'].values
 
-# Train the classifier on the training data
-clf.fit(X_train, y_train)
+    # Split the dataset into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=r_s)
 
-# Use the trained classifier to make predictions on the test data
-y_pred = clf.predict(X_test)
+    # Create a decision tree classifier
+    clf = DecisionTreeClassifier()
 
-# Measure the accuracy of the classifier
-accuracy = accuracy_score(y_test, y_pred)
-print(f"Accuracy: {accuracy}")
+    # Train the classifier on the training data
+    clf.fit(X_train, y_train)
+
+    # Use the trained classifier to make predictions on the test data
+    y_pred = clf.predict(X_test)
+
+    # display confusion matrix and ROC curve
+    display_confusion_matrix(y_test, y_pred)
+    # display_roc_curve(clf, X_train, y_train, X_test, y_test)
+
+    # Measure the accuracy of the classifier
+    accuracy = accuracy_score(y_test, y_pred)
+
+    # save results
+    dict_to_append = {"Accuracy" : accuracy, "VariablesUsed" : variables[:], "RandomStateUsed" : r_s}
+
+    # concatenate the new DataFrame with the original DataFrame
+    ml_stats_df.loc[len(ml_stats_df.index)] = dict_to_append
+    return ml_stats_df
+
+
+stats_df = pd.read_csv("../data/ml_stats.csv")
+for i in range(50, 150):
+    k = random.randint(1, len(variables))
+    vars_used = random.sample(variables, k)
+    stats_df = test_ipts_decision_tree(data, vars_used, i, stats_df)
+
+stats_df.to_csv("../data/ml_stats.csv")
