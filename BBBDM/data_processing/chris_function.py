b4e9471237598e8059242cb9720c727d49ad067a
christopherMaherCS2 <93490756+christopherMaherCS@users.noreply.github.com>
2023-09-25 20:03:13 -0500

    Partial update

--- BBBDM/data_processing/chris_function.py
+++ BBBDM/data_processing/chris_function.py
@@ -12,6 +12,7 @@ def join_dataframe_firmid(*dataframes:pd.DataFrame) -> pd.DataFrame:
         x = dataframes[0]['FirmID']
     except Exception as e:
         return f"Expection {e} occurred, did the dataframes have FirmID?"
+    
     df = pd.join(dataframes, on='FirmID', how='outer')
     df = df.loc[:,~df.columns.duplicated()]
     return df
--- BBBDM/test/test_merge_data_frames.py
+++ BBBDM/test/test_merge_data_frames.py
@@ -1,14 +1,8 @@
 """
 Test merging dataframes based on firmid"""
-import sys
-from pathlib import Path
-data_processing_path = str(Path(__file__).resolve().parent.parent / 'data_processing')
 
-# Append the 'data_processing' directory to sys.path
-sys.path.append(data_processing_path)
-#from data_processing import chris_function
+from data_processing import chris_function
 import pandas as pd
-print(sys.path)
 
 
 def test_join_dataframe_firmid_multiple_success():
@@ -28,3 +22,19 @@ def test_join_dataframe_firmid_multiple_success():
 
     # Compare
     assert actual.equals(expected)
+
+def test_join_dataframe_firmid_failed():
+    "Test what would happen if the FirmID failed"
+    # Input dataframes
+    df1 = pd.DataFrame({'FirmIDD': [1, 2, 3], 'Name': ['A', 'B', 'C']})
+    df2 = pd.DataFrame({'FirmIDD': [1, 2, 3], 'Business_type': ['Construction', 'Construction', 'Something']})
+    df3 = pd.DataFrame({'FirmIDD': [1, 2, 3], 'Location': ['USA', 'USA', 'USA']})
+
+    #Expected output
+    expected = "Expection 'FirmID' occurred, did the dataframes have FirmID?"
+
+    # Actual output
+    actual = chris_function.join_dataframe_firmid(df1, df2, df3)
+
+    # Compare
+    assert actual == expected
