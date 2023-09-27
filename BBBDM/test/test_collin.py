5d0ea93e22589f7565ece2f66a6bd014c0f5c096
Collin Dahlback <cdahlback208@icloud.com>
2023-09-26 21:54:53 -0500

    small changes to my function
    - Business Leins info was not what we expected
    - Modified function to reflect those column names
    - renamed test file as well for pytest

---
+++ BBBDM/test/test_collin.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import pytest
+from BBBDM.data_processing.Collin import compare_dataframes  # Import your actual function here
+
+
+# Define a test case for a passing case in compare_dataframes
+def test_compare_dataframes_pass():
+    # Create a sample historicalData DataFrame
+    historicalData = pd.DataFrame({
+        'Firm_id': [1, 2, 3],
+        'BusinessName': ['XYZ Corp', 'LMN Ltd', 'ABC Inc'],
+        'Active': [True, False, True],
+        'Address': ['123 Main St', '789 Oak St', '456 Elm St'],
+        'Zip Code': ['12345', '67890', '54321']
+    })
+
+    # Create a sample newData DataFrame with matching and non-matching rows
+    newData = pd.DataFrame({
+        'Business Name': ['XYZ Corp', 'PQR Corp', 'ABC Inc'],
+        'Address 1': ['123 Main St', '777 Maple St', '456 Elm St'],
+        'Zip Code': ['12345', '99999', '54321'],
+        'Business Filing Type': ['Type A', 'Type B', 'Type C'],
+        'Filing Date': ['2022-01-01', '2022-02-02', '2022-03-03'],
+        'Status': ['Active', 'Inactive', 'Active'],
+        'Address 2': ['Suite 100', '', 'Apt 2B'],
+        'City': ['City1', 'City2', 'City3'],
+        'Region Code': ['NY', 'CA', 'TX'],
+        'Party Full Name': ['John Doe', 'Jane Smith', 'Bob Johnson'],
+        'Next Renewal Due Date': ['2023-01-01', '2023-02-02', '2023-03-03']
+    })
+
+    # Call your function to find matching datapoints
+    result_df = compare_dataframes(historicalData, newData)
+
+    # Check if the result is a DataFrame
+    assert isinstance(result_df, pd.DataFrame)
+
+    # Check if the result has the expected columns
+    expected_columns = ['Firm_id', 'BusinessName', 'MatchesAddress', 'Address_new', 'MatchesZip', 'Zip Code_new',
+                        'Business Filing Type', 'Filing Date', 'Status', 'Address 2', 'City', 'Region Code',
+                        'Party Full Name', 'Next Renewal Due Date']
+    assert result_df.columns.tolist() == expected_columns
+
+
+# Define a test case for a failing case in compare_dataframes
+def test_compare_dataframes_fail():
+    # Create two sample DataFrames with non-overlapping data
+    historicalData = pd.DataFrame({
+        'Firm_id': [1, 2, 3],
+        'BusinessName': ['XYZ Corp', 'LMN Ltd', 'ABC Inc'],
+        'Active': [True, False, True],
+        'Address': ['123 Main St', '789 Oak St', '456 Elm St'],
+        'Zip Code': ['12345', '67890', '54321']
+    })
+
+    # Create a sample newData DataFrame which is empty
+    newData = pd.DataFrame({})
+
+    # Call your function to find matching datapoints
+    result_df = compare_dataframes(historicalData, newData)
+
+    # Check if the result is an empty DataFrame
+    assert result_df.empty
